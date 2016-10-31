122d48306e7fa191feaa931f0573de5fdbd2acef
unknown <gevorkyan_ag@a23_03.zp.itstep.org>
2016-10-31 13:13:42 +0200

    MyComm3

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace MyGit
         {
             fish f = new fish() { Name = "fish1", Weight=100, Price=200};
             Console.WriteLine(f.ToString());
+            fish f2 = new fish() { Name = "fish2", Weight = 200, Price = 300 };
+            Console.WriteLine(f2.ToString());
         }
     }
 }
